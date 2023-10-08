e13c2e68dafcb93fccea8a8b042b2d4f1444e445
BehshadBabai <behshad.babai@gmail.com>
2023-10-08 04:23:41 -0700

    added content to feedback page

--- src/Pages/Feedback.tsx
+++ src/Pages/Feedback.tsx
@@ -1,7 +1,108 @@
 import React from 'react';
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Rate,
+  Row,
+  Select,
+  Space,
+  Typography
+} from 'antd';
 
-const Feedback: React.FC = () => {
-  return <></>;
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!'
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}'
+  }
 };
 
-export default Feedback;
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { span: 19 }
+};
+
+const onFinish = (values: any) => {
+  // send email later on
+  console.log(values);
+};
+
+const FeedBack: React.FC = () => (
+  <Space direction='vertical' size={'large'} style={{ width: '100%' }}>
+    <Row justify={'center'}>
+      <Col xs={24} md={19} lg={14} style={{ width: '90%' }}>
+        <Row>
+          <Col span={5}></Col>
+          <Col span={19}>
+            <Typography.Text
+              style={{
+                textAlign: 'center',
+                fontSize: '1.1em'
+              }}
+            >
+              Send Us Your Feedback:
+            </Typography.Text>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+    <Row justify={'center'}>
+      <Col xs={24} md={19} lg={14}>
+        <Form
+          {...layout}
+          name='nest-messages'
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+          initialValues={{ usage: 'patient', rate: 1 }}
+        >
+          <Form.Item
+            name={['user', 'name']}
+            label='Name'
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={['user', 'email']}
+            label='Email'
+            rules={[{ type: 'email', required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name='usage' label='Usage'>
+            <Select>
+              <Option value='patient'>Patient</Option>
+              <Option value='dentist'>Dentist</Option>
+              <Option value='other'>Other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name='rate' label='Rate'>
+            <Rate allowClear={false} />
+          </Form.Item>
+          <Form.Item name={['user', 'message']} label='Message'>
+            <Input.TextArea style={{ height: '200px' }} />
+          </Form.Item>
+          <Form.Item wrapperCol={{ span: 24 }}>
+            <Row>
+              <Col span={4}></Col>
+              <Col>
+                <Button type='primary' htmlType='submit'>
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  </Space>
+);
+
+export default FeedBack;
